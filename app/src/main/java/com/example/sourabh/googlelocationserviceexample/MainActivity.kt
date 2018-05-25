e5cd27f4ef661d1008b204e776ff8fe9fbd4dd54
Sourabh S. Rawat <dragneelfps@gmail.com>
2018-05-25 11:41:01 +0530

    Added functionality for starting and stopping location updates

--- app/src/main/java/com/example/sourabh/googlelocationserviceexample/MainActivity.kt
+++ app/src/main/java/com/example/sourabh/googlelocationserviceexample/MainActivity.kt
@@ -1,12 +1,190 @@
 package com.example.sourabh.googlelocationserviceexample
 
+import android.app.Activity
+import android.content.Intent
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+private val GOOGLE_SERVICE_DIALOG: Int = 1231
+private val LOCATION_SETTINGS_REQ: Int = 34321
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private var mShouldRequestUpdates: Boolean = false
+
+    /**
+     * Toggles location update button and returns true if toggled to getting updates
+     */
+    fun toggleButton(): Boolean{
+        mShouldRequestUpdates = !mShouldRequestUpdates
+        toggleUpdateBtn.apply {
+            text = when (mShouldRequestUpdates) {
+                true -> {
+                    "Stop Location Updates"
+                }
+                false -> {
+                    "Start Location Updates"
+                }
+            }
+        }
+        return mShouldRequestUpdates
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GOOGLE_SERVICE_DIALOG -> {
+                Toast.makeText(this, "Got result from google service check", Toast.LENGTH_SHORT).show()
+                when (resultCode) {
+                    Activity.RESULT_OK -> Toast.makeText(this, "Good Work", Toast.LENGTH_SHORT).show()
+                    Activity.RESULT_CANCELED -> Toast.makeText(this, "Bad Work", Toast.LENGTH_SHORT).show()
+                }
+            }
+            LOCATION_SETTINGS_REQ -> {
+                Toast.makeText(this, "Got result from location settings change request", Toast.LENGTH_SHORT).show()
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        Toast.makeText(this, "Location settings changed", Toast.LENGTH_SHORT).show()
+                        initDefaultUI()
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        Toast.makeText(this, "Location settings weren't changed", Toast.LENGTH_SHORT).show()
+                        initNullUI()
+                    }
+                }
+            }
+            LOCATION_PERMISSION_REQ -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        initDefaultUI()
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        initNullUI()
+                    }
+                }
+            }
+        }
+    }
+
+    private lateinit var mLocationRequest: LocationRequest
+
+    private val LOCATION_PERMISSION_REQ: Int = 143
+
+    override fun onResume() {
+        super.onResume()
+        if(hasPermssion(this)) {
+            hasGooglePlayService2(this).let { result ->
+                when (result) {
+                    ConnectionResult.SUCCESS -> {
+                        mLocationRequest = createLocationRequest(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                        val settingsTask = createSettingsCheckTask(this, getLocationBuilder(mLocationRequest))
+                        settingsTask.addOnSuccessListener { locationSettingsResponse ->
+                            initDefaultUI()
+                        }
+                        settingsTask.addOnFailureListener { exception ->
+                            if (exception is ResolvableApiException) {
+                                exception.startResolutionForResult(this, LOCATION_SETTINGS_REQ)
+                            }
+                        }
+                    }
+                    else -> {
+                        initNullUI()
+                        val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, result, GOOGLE_SERVICE_DIALOG)
+                        dialog.show()
+                    }
+                }
+            }
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ)
+        }
+    }
+
+    private val toggleButtonListener: View.OnClickListener = View.OnClickListener { v ->
+        if(toggleButton()){
+            startLocationUpdates()
+        }else{
+            stopLocationUpdates()
+        }
+    }
+
+    fun stopLocationUpdates(){
+        fusedLocationProvider.removeLocationUpdates(mLocationCallback)
+    }
+
+    private val mLocationCallback: LocationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult?.run {
+                if(locations.isEmpty())
+                    return
+                for(location in locations){
+                    updateUI(location)
+                }
+            }
+        }
+    }
+
+    private fun updateUI(location: Location?) {
+        location?.let {
+            time.text = it.time.toString()
+            latitude.text = it.latitude.toString()
+            longitude.text = it.longitude.toString()
+        }
+    }
+
+
+    fun startLocationUpdates(){
+        fusedLocationProvider.requestLocationUpdates(mLocationRequest, mLocationCallback, null)
+    }
+
+    private fun displayLastKnownLocation(){
+        fusedLocationProvider.lastLocation.addOnSuccessListener { location ->
+            if(location == null){
+                initNullUI(enableButton = true)
+            }else{
+                updateUI(location)
+            }
+        }.addOnFailureListener { exception ->
+
+        }
+    }
+
+    private lateinit var fusedLocationProvider: FusedLocationProviderClient
+
+    private fun init(){
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun initDefaultUI(){
+        val nl = ""
+        time.text = nl
+        latitude.text = nl
+        longitude.text = nl
+        toggleUpdateBtn.isEnabled = true
+        displayLastKnownLocation()
+        toggleUpdateBtn.setOnClickListener(toggleButtonListener)
+    }
+
+    private fun initNullUI(enableButton: Boolean = false){
+        val nl = "null"
+        time.text = nl
+        latitude.text = nl
+        longitude.text = nl
+        toggleUpdateBtn.isEnabled = enableButton
+        toggleUpdateBtn.setOnClickListener(null)
     }
 }
--- app/src/main/java/com/example/sourabh/googlelocationserviceexample/Util.kt
+++ app/src/main/java/com/example/sourabh/googlelocationserviceexample/Util.kt
@@ -1,9 +1,37 @@
 package com.example.sourabh.googlelocationserviceexample
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.tasks.Task
+
+/**
+ * Creates and returns a location request object with specified priority and interval rate
+ */
+fun createLocationRequest(priorityParam: Int = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, intervalParam: Long = 5000): LocationRequest{
+    return  LocationRequest().apply {
+        interval = intervalParam
+        fastestInterval = intervalParam
+        priority = priorityParam
+    }
+}
+
+fun getLocationBuilder(locationRequest: LocationRequest): LocationSettingsRequest.Builder {
+    return LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+}
+
+fun createSettingsCheckTask(context: Context, builder: LocationSettingsRequest.Builder) : Task<LocationSettingsResponse> {
+    val client = LocationServices.getSettingsClient(context)
+    return client.checkLocationSettings(builder.build())
+}
 
 fun hasGooglePlayService(context: AppCompatActivity): Boolean{
     val instance = GoogleApiAvailability.getInstance()
@@ -15,6 +43,10 @@ fun hasGooglePlayService(context: AppCompatActivity): Boolean{
             false
         }
     }
+}
+fun hasGooglePlayService2(context: AppCompatActivity): Int{
+    val instance = GoogleApiAvailability.getInstance()
+    return instance.isGooglePlayServicesAvailable(context)
 }
 
 fun parseSpeed(location: Location): String {
@@ -39,4 +71,10 @@ fun parseAltitude(location: Location): String? {
 
 fun hasSdk(requiredSdkVersion: Int): Boolean {
     return android.os.Build.VERSION.SDK_INT >= requiredSdkVersion
+}
+
+
+
+fun hasPermssion(context: Context): Boolean{
+    return ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
